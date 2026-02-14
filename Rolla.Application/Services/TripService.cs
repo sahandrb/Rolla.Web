8641fb07a3661cd40d37ae48804513c22c26740b
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2026-02-14 12:58:03 +0330

    TripService

---
+++ Rolla.Application/Services/TripService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetTopologySuite;
+using NetTopologySuite.Geometries;
+using Rolla.Application.DTOs.Trip;
+using Rolla.Application.Interfaces;
+using Rolla.Domain.Entities;
+using Rolla.Domain.Enums;
+
+
+
+namespace Rolla.Application.Services;
+
+public class TripService : ITripService
+{
+    private readonly IApplicationDbContext _context;
+
+    public TripService(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> CreateTripAsync(CreateTripDto dto, string riderId)
+    {
+        // ایجاد کارخانه برای ساخت نقاط جغرافیایی با استاندارد GPS (SRID 4326)
+        var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+
+        // تبدیل اعداد اعشاری به مدل مختصاتی Point
+        var originPoint = geometryFactory.CreatePoint(new Coordinate(dto.OriginLng, dto.OriginLat));
+        var destPoint = geometryFactory.CreatePoint(new Coordinate(dto.DestinationLng, dto.DestinationLat));
+
+        var trip = new Trip
+        {
+            RiderId = riderId,
+            Origin = originPoint,
+            Destination = destPoint,
+            Price = dto.EstimatedPrice,
+            Status = TripStatus.Searching, // وضعیت شروع: در حال جستجو برای راننده
+            CreatedAt = DateTime.UtcNow
+        };
+
+        _context.Trips.Add(trip);
+        await _context.SaveChangesAsync();
+
+        return trip.Id; // برگرداندن آیدی سفر برای استفاده در SignalR یا مانیتورینگ
+    }
+}
--- Rolla.Application/obj/Debug/net9.0/Rolla.Application.AssemblyInfo.cs
+++ Rolla.Application/obj/Debug/net9.0/Rolla.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rolla.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a3ba471078a5d109182b5416424843658ee047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe79ac4fa32c2372ba583ce8273336a5c557797")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rolla.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rolla.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

77a3ba471078a5d109182b5416424843658ee047
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2026-02-14 12:38:04 +0330

    Clone

--- Rolla.Domain/obj/Debug/net9.0/Rolla.Domain.AssemblyInfo.cs
+++ Rolla.Domain/obj/Debug/net9.0/Rolla.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rolla.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ff747b651cd732747df9a19a74628970adc3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rolla.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rolla.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

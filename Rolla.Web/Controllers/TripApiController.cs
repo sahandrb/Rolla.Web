8c1d1bbf56ff7215b4e08b7bbd4c87d09c07b1a7
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2026-02-14 13:02:20 +0330

    TripApiController

---
+++ Rolla.Web/Controllers/TripApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rolla.Application.DTOs.Trip;
+using Rolla.Application.Interfaces;
+using System.Security.Claims;
+
+namespace Rolla.Web.Controllers;
+
+[Authorize] // فقط کاربران لاگین شده
+[ApiController]
+[Route("api/[controller]")]
+public class TripApiController : ControllerBase
+{
+    private readonly ITripService _tripService;
+
+    public TripApiController(ITripService tripService)
+    {
+        _tripService = tripService;
+    }
+
+    [HttpPost("request")]
+    public async Task<IActionResult> RequestTrip([FromBody] CreateTripDto dto)
+    {
+        // پیدا کردن آیدی کاربری که لاگین کرده است
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (userId == null) return Unauthorized();
+
+        var tripId = await _tripService.CreateTripAsync(dto, userId);
+
+        return Ok(new { Message = "سفر با موفقیت ثبت شد و در انتظار راننده است", TripId = tripId });
+    }
+}
+

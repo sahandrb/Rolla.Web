5a83379e2b60c7b31c2b9e3a088e7645926c029e
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2026-02-15 01:21:10 +0330

    ApplicationUser

--- Rolla.Domain/Entities/ApplicationUser.cs
+++ Rolla.Domain/Entities/ApplicationUser.cs
@@ -1,19 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Rolla.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 
 namespace Rolla.Domain.Entities
 {
 
     public class ApplicationUser : IdentityUser
     {
-        public string? FullName { get; set; } = default!;
-        // فیلدهای اختصاصی بیزنس شما
-        public bool IsDriver { get; set; }
-        public string? CarModel { get; set; }  // فقط برای راننده
-        public string? PlateNumber { get; set; } // پلاک ماشین
+        public string? FullName { get; set; }
+        public bool IsDriver { get; set; } // این فیلد قدیمی است اما نگهش دار
+        public DriverStatus DriverStatus { get; set; } = DriverStatus.None; // ✨ فیلد جدید
+
+        public string? CarModel { get; set; }
+        public string? PlateNumber { get; set; }
     }
 }

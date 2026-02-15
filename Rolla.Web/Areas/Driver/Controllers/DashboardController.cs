97f894bbe84afc0db0fd8f24c8b0adaba4d97ffd
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2026-02-15 17:30:38 +0330

    make driver controller

---
+++ Rolla.Web/Areas/Driver/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Rolla.Domain.Entities;
+using Rolla.Domain.Enums;
+
+namespace Rolla.Web.Areas.Driver.Controllers
+{
+    [Area("Driver")]
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DashboardController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            // سناریوی چک کردن وضعیت
+            if (user.DriverStatus == DriverStatus.Pending)
+            {
+                return View("Pending"); // صفحه‌ای که می‌گوید "در حال بررسی..."
+            }
+
+            if (user.DriverStatus != DriverStatus.Approved)
+            {
+                return RedirectToAction("Register"); // اگر هنوز ثبت نام نکرده
+            }
+
+            return View(); // فایل WorkBoard.cshtml
+        }
+    }
+}

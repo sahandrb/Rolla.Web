22bcf67cc1040069f02765f33913eb91c9cc47e2
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2026-02-15 18:39:36 +0330

    IPricingService

--- Rolla.Application/obj/Debug/net9.0/Rolla.Application.AssemblyInfo.cs
+++ Rolla.Application/obj/Debug/net9.0/Rolla.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rolla.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f894bbe84afc0db0fd8f24c8b0adaba4d97ffd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a703c13633ba5d19731a19c4b77e91145c148c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rolla.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rolla.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
